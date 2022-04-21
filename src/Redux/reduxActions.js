c4eeef8763ec348c304bb460a44566b9fc5a1a29
manuochoa <manu_8a@outlook.com>
2022-04-21 15:49:40 +0200

    fix referral

--- src/Redux/reduxActions.js
+++ src/Redux/reduxActions.js
@@ -45,7 +45,7 @@ let provider = new ethers.providers.JsonRpcProvider(
 
 // let vaultAddress = "0x81712e6280FC625fb09E3714B261Bd5163DAba2b";
 let tokenAddress = "0x34F7dE3336F30545c05832FA7b067ebC2242F2cc";
-let faucetAddress = "0x7C00EB9E083f0bA7c241230f4C78481DB2919727";
+let faucetAddress = "0x4d39e6fB4a69f8495d519041219469B613271220";
 let pancakeRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
 
 let faucetInstance = new ethers.Contract(faucetAddress, faucetABI, provider);
